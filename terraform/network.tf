d8b24c1d9db360b2c755a1b79b3c7a54c1dfc290
Mine <mine@MacBookPro.lan>
2025-09-13 18:42:06 -0500

    fix

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,7 +1,7 @@
-module "network" {
-  source  = "app.terraform.io/<ORG>/network/azure"
-  version = "0.1.0"
-
+module "azure-network" {
+  source  = "app.terraform.io/solexb/azure-network/azurerm"
+  version = "0.0.1-alpha-2"
+  
   project             = var.project
   resource_group_name = local.resource_group_name
   location            = azurerm_resource_group.rg.location
--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,8 +1,20 @@
-module "linux_vm" {
-  source  = "app.terraform.io/<ORG>/linux-vm/azure"
-  version = "0.1.0"
+# module "linux_vm" {
+#   source  = "app.terraform.io/<ORG>/linux-vm/azure"
+#   version = "0.1.0"
 
-  project             = var.project
+#   project             = var.project
+#   resource_group_name = local.resource_group_name
+#   location            = azurerm_resource_group.rg.location
+#   nic_id              = module.network.nic_id
+#   vm_size             = "Standard_B1s"
+#   admin_username      = "azureuser"
+#   ssh_public_key      = var.ssh_public_key
+# }
+
+module "azure-linux-vm" {
+  source  = "app.terraform.io/solexb/azure-linux-vm/azurerm"
+  version = "0.0.1-alpha"
+  
   resource_group_name = local.resource_group_name
   location            = azurerm_resource_group.rg.location
   nic_id              = module.network.nic_id
