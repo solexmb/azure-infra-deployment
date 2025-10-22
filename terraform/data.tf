1dce536b50f38cd21bac32bbf7cde56099ab6ef0
Mine <mine@MacBookPro.lan>
2025-10-22 14:37:29 -0500

    fix

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,5 +1,5 @@
-data "azurerm_log_analytics_workspace" "law" {
-  count               = var.enable_monitoring ? 1 : 0
-  name                = "cloud-platform-cu-law"
-  resource_group_name = "cloud-platform-cu-rg"
-}
+# data "azurerm_log_analytics_workspace" "law" {
+#   count               = var.enable_monitoring ? 1 : 0
+#   name                = "cloud-platform-cu-law"
+#   resource_group_name = "cloud-platform-cu-rg"
+# }
