7398e8a4fa4b9bed75ffa475fc0d5424817dfe0a
Mine <mine@MacBookPro.lan>
2025-10-31 12:45:43 -0500

    grafana

---
+++ terraform/grafana.tf
@@ -0,0 +1,27 @@
+# data "azurerm_subscription" "current" {}
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_dashboard_grafana" "example" {
+  name                              = "test-grafana"
+  resource_group_name               = azurerm_resource_group.primary_automation_dev_rg.name
+  location                          = azurerm_resource_group.primary_automation_dev_rg.location
+  grafana_major_version             = 11
+  api_key_enabled                   = true
+  deterministic_outbound_ip_enabled = true
+  public_network_access_enabled     = false
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    env = "dev"
+  }
+}
+
+resource "azurerm_role_assignment" "grafana_monitoring_reader" {
+#   scope                = data.azurerm_subscription.current.id
+  scope                = data.azurerm_client_config.current.subscription_id
+  role_definition_name = "Monitoring Reader"
+  principal_id         = azurerm_dashboard_grafana.grafana.identity[0].principal_id
+}
--- terraform/vm.tf
+++ terraform/vm.tf
@@ -11,81 +11,81 @@
 #   ssh_public_key      = var.ssh_public_key
 # }
 
-module "azure-linux-vm" {
-  source  = "app.terraform.io/solexb/azure-linux-vm/azurerm"
-  version = "0.1.0-alpha-8"
+# module "azure-linux-vm" {
+#   source  = "app.terraform.io/solexb/azure-linux-vm/azurerm"
+#   version = "0.1.0-alpha-8"
   
-  vm_name = "${local.primary_prefix}-demo" 
-#   project             = var.project
-  resource_group_name = azurerm_resource_group.primary_automation_dev_rg.name
-  location            = azurerm_resource_group.primary_automation_dev_rg.location
-  nic_id  = module.azure-network.nic_ids["demo1"]
-  # nic_id              = module.azure-network.nic_id
-  vm_size             = "Standard_B1s"
-  admin_username      = "azureuser"
-  ssh_public_key      = var.ssh_public_key
+#   vm_name = "${local.primary_prefix}-demo" 
+# #   project             = var.project
+#   resource_group_name = azurerm_resource_group.primary_automation_dev_rg.name
+#   location            = azurerm_resource_group.primary_automation_dev_rg.location
+#   nic_id  = module.azure-network.nic_ids["demo1"]
+#   # nic_id              = module.azure-network.nic_id
+#   vm_size             = "Standard_B1s"
+#   admin_username      = "azureuser"
+#   ssh_public_key      = var.ssh_public_key
 
-  enable_monitoring   = false
-  # log_analytics_workspace_id = data.azurerm_log_analytics_workspace.law.id
+#   enable_monitoring   = false
+#   # log_analytics_workspace_id = data.azurerm_log_analytics_workspace.law.id
 
-  # email_action_receivers = [
-  #   {
-  #     name          = "OpsTeam"
-  #     email_address = "solomon.balogun.m@gmail.com"
-  #   },
-  #   {
-  #     name          = "DevTeam"
-  #     email_address = "dev@example.com"
-  #   }
-  # ]
-  identity = {
-    type         = "SystemAssigned"
-    identity_ids = []
-  }
-#   tags = {
-#     Environment = "dev"
-#     Project     = "demo"
+#   # email_action_receivers = [
+#   #   {
+#   #     name          = "OpsTeam"
+#   #     email_address = "solomon.balogun.m@gmail.com"
+#   #   },
+#   #   {
+#   #     name          = "DevTeam"
+#   #     email_address = "dev@example.com"
+#   #   }
+#   # ]
+#   identity = {
+#     type         = "SystemAssigned"
+#     identity_ids = []
 #   }
+# #   tags = {
+# #     Environment = "dev"
+# #     Project     = "demo"
+# #   }
 
 
-}
+# }
 #
 
-module "azure-linux-vm-1" {
-  source  = "app.terraform.io/solexb/azure-linux-vm/azurerm"
-  version = "0.1.0-alpha-7"
+# module "azure-linux-vm-1" {
+#   source  = "app.terraform.io/solexb/azure-linux-vm/azurerm"
+#   version = "0.1.0-alpha-7"
   
-  vm_name = "${local.primary_prefix}-demo-2" 
-#   project             = var.project
-  resource_group_name = azurerm_resource_group.primary_automation_dev_rg.name
-  location            = azurerm_resource_group.primary_automation_dev_rg.location
-  nic_id              = module.azure-network.nic_ids["demo2"]
-  # nic_id              = module.azure-network-1.nic_id
-  vm_size             = "Standard_B1s"
-  admin_username      = "azureuser"
-  ssh_public_key      = var.ssh_public_key
+#   vm_name = "${local.primary_prefix}-demo-2" 
+# #   project             = var.project
+#   resource_group_name = azurerm_resource_group.primary_automation_dev_rg.name
+#   location            = azurerm_resource_group.primary_automation_dev_rg.location
+#   nic_id              = module.azure-network.nic_ids["demo2"]
+#   # nic_id              = module.azure-network-1.nic_id
+#   vm_size             = "Standard_B1s"
+#   admin_username      = "azureuser"
+#   ssh_public_key      = var.ssh_public_key
 
-  enable_monitoring   = true
-  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.law.id
+#   enable_monitoring   = true
+#   log_analytics_workspace_id = data.azurerm_log_analytics_workspace.law.id
 
-  email_action_receivers = [
-    {
-      name          = "OpsTeam"
-      email_address = "solomon.balogun.m@gmail.com"
-    },
-    {
-      name          = "DevTeam"
-      email_address = "dev@example.com"
-    }
-  ]
-  identity = {
-    type         = "SystemAssigned"
-    identity_ids = []
-  }
-#   tags = {
-#     Environment = "dev"
-#     Project     = "demo"
+#   email_action_receivers = [
+#     {
+#       name          = "OpsTeam"
+#       email_address = "solomon.balogun.m@gmail.com"
+#     },
+#     {
+#       name          = "DevTeam"
+#       email_address = "dev@example.com"
+#     }
+#   ]
+#   identity = {
+#     type         = "SystemAssigned"
+#     identity_ids = []
 #   }
+# #   tags = {
+# #     Environment = "dev"
+# #     Project     = "demo"
+# #   }
 
 
-}
+# }
