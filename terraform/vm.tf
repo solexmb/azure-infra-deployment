d92f74694852c1b39c1bb96467c084332c64d5a5
Mine <mine@MacBookPro.lan>
2025-11-03 16:42:51 -0600

    fix

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -13,7 +13,7 @@
 
 module "azure-linux-vm" {
   source  = "app.terraform.io/solexb/azure-linux-vm/azurerm"
-  version = "0.1.0-alpha-16"
+  version = "0.1.0-alpha-17"
   
   vm_name = "${local.primary_prefix}-demo" 
 #   project             = var.project
