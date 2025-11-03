5947e8fba5aa6da8c6833b14b0547136a3c85465
Mine <mine@MacBookPro.lan>
2025-11-03 12:39:33 -0600

    service-map

--- terraform/law.tf
+++ terraform/law.tf
@@ -1,14 +1,14 @@
-# resource "azurerm_resource_group" "primary_automation_cu_rg" {
-#   name     = "${local.prefix}-cu-rg"
-#   location = var.location
-# }
+resource "azurerm_resource_group" "primary_automation_cu_rg" {
+  name     = "${local.prefix}-cu-rg"
+  location = var.location
+}
 
-# // Log Analytics Workspace
-# resource "azurerm_log_analytics_workspace" "vm_law" {
-#   name                = "${local.prefix}-cu-law"
-#   location            = azurerm_resource_group.primary_automation_cu_rg.location
-#   resource_group_name = azurerm_resource_group.primary_automation_cu_rg.name
-#   sku                 = "PerGB2018"
-#   retention_in_days   = 30
-# #   tags                = var.tags
-# }
+// Log Analytics Workspace
+resource "azurerm_log_analytics_workspace" "vm_law" {
+  name                = "${local.prefix}-cu-law"
+  location            = azurerm_resource_group.primary_automation_cu_rg.location
+  resource_group_name = azurerm_resource_group.primary_automation_cu_rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+#   tags                = var.tags
+}
