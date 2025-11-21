d48f3bfdbb8352da0c675b72781e3bc9e398e925
Mine <mine@MACs-MacBook-Pro.local>
2025-11-21 05:23:38 -0600

    add

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,4 +1,4 @@
-# data "azurerm_log_analytics_workspace" "law" {
-#   name                = "cloud-platform-cu-law"
-#   resource_group_name = "cloud-platform-cu-rg"
-# }
+data "azurerm_log_analytics_workspace" "law" {
+  name                = "cloud-platform-cu-law"
+  resource_group_name = "cloud-platform-cu-rg"
+}
